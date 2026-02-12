818dbeed868ff23abeedd6f802945f89a7c9b476
francesco.fucci <frfucci@protonmail.ch>
2026-02-12 17:23:12 +0400

    Small clean up to the array ringbuffer.

--- lib/include/concurrency/array_ringbuffer.hpp
+++ lib/include/concurrency/array_ringbuffer.hpp
@@ -9,30 +9,9 @@ class ArrayRingBuffer
     static_assert(N > 0, "Array buffer needs to have at least one slot");
     static_assert((N & (N - 1)) == 0, "Array buffer needs to be a power of 2");
 
-    bool push(const T& value) noexcept
-    {
-        auto index = write_index_.load(std::memory_order_relaxed);
-        auto next_index = (index + 1) & (N - 1);
-        if (next_index == read_index_.load(std::memory_order_acquire)) {
-            return false;
-        }
+    bool push(const T& value) noexcept;
 
-        buffer_[index] = value;
-        write_index_.store(next_index, std::memory_order_release);
-        return true;
-    }
-
-    bool pop(T& value) noexcept
-    {
-        auto tail = read_index_.load(std::memory_order_relaxed);
-        if (tail == write_index_.load(std::memory_order_acquire)) {
-            return false;
-        }
-
-        value = buffer_[tail];
-        read_index_.store((tail + 1) & (N - 1), std::memory_order_release);
-        return true;
-    }
+    bool pop(T& value) noexcept;
 
     [[nodiscard]] size_t read_idx() const noexcept
     {
@@ -49,3 +28,32 @@ class ArrayRingBuffer
     alignas(64) T buffer_[N];
     alignas(64) std::atomic<size_t> write_index_{0};
 };
+
+template <typename T, std::size_t N>
+bool ArrayRingBuffer<T, N>::push(const T& value) noexcept
+{
+    auto index = write_index_.load(std::memory_order_relaxed);
+    auto next_index = (index + 1) & (N - 1);
+    // The queue is full
+    if (next_index == read_index_.load(std::memory_order_acquire)) {
+        return false;
+    }
+
+    buffer_[index] = value;
+    write_index_.store(next_index, std::memory_order_release);
+    return true;
+}
+
+template <typename T, std::size_t N>
+bool ArrayRingBuffer<T, N>::pop(T& value) noexcept
+{
+    auto tail = read_index_.load(std::memory_order_relaxed);
+    // The queue is empty
+    if (tail == write_index_.load(std::memory_order_acquire)) {
+        return false;
+    }
+
+    value = buffer_[tail];
+    read_index_.store((tail + 1) & (N - 1), std::memory_order_release);
+    return true;
+}
